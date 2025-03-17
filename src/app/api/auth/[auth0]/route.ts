250a53754e9f3e8d69226510911ae5b0bb92043e
Viannis <murphycaxton360@gmail.com>
2025-03-17 20:42:30 +0530

    Handle Auth

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,11 +1,12 @@
-import { handleAuth, handleLogout } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin, handleLogout } from "@auth0/nextjs-auth0";
 
-const logoutUrl = [
-  `${process.env.AUTH0_ISSUER_BASE_URL}/v2/logout?`,
-  `client_id=${process.env.AUTH0_CLIENT_ID}`,
-  `&returnTo=${process.env.AUTH0_BASE_URL}`,
-];
-
-export const GET = handleAuth({
-  logout: handleLogout({ returnTo: logoutUrl.join("") }),
+export default handleAuth({
+  // This login handler performs a full redirect to Auth0’s /authorize endpoint
+  login: handleLogin({
+    returnTo: process.env.AUTH0_BASE_URL, // After login, redirect back to your app
+  }),
+  logout: handleLogout({
+    // Build a logout URL that logs out from Auth0 and then returns to your app
+    returnTo: `${process.env.AUTH0_ISSUER_BASE_URL}/v2/logout?client_id=${process.env.AUTH0_CLIENT_ID}&returnTo=${process.env.AUTH0_BASE_URL}`,
+  }),
 });
