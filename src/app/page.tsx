4bf51779bb376d9ec1efea65ef1c25a70c32328c
Viannis <murphycaxton360@gmail.com>
2025-03-20 02:24:46 +0530

    Updated Home page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { useEffect, useState } from "react";
+import hctlogo from "@/app/1hctlogo.png";
+import Image from "next/image";
 import Link from "next/link";
-import { Layout, Menu, Button, notification } from "antd";
+import { Layout, Button, notification } from "antd";
 import axios from "axios";
 import type { NotificationArgsProps } from "antd";
 
@@ -12,6 +14,13 @@ type NotificationPlacement = NotificationArgsProps["placement"];
 type NotificationType = "success" | "info" | "warning" | "error";
 
 const { Header } = Layout;
+
+type ButtonProps = {
+  paddingInline: number | string;
+  paddingTop: number | string;
+  paddingBottom: number | string;
+  fontSize: string | number;
+};
 
 // The Header component from Ant Design is used to create a navigation bar at the top of the page.
 
@@ -111,21 +120,40 @@ export default function Home() {
 
   // This effect checks the user's account setup status and role, setting the appropriate state for navigation.
 
-  const renderButton = () => {
+  const renderButton = (buttonProps: ButtonProps) => {
     console.log("User:", user);
     console.log("Role:", role);
     console.log("Account setup incomplete:", accountSetupIncomplete);
     if (!buttonLoading) {
       if (!user) {
         return (
-          <Button type="primary" onClick={handleLogin}>
+          <Button
+            shape="round"
+            style={{
+              paddingInline: buttonProps.paddingInline,
+              paddingTop: buttonProps.paddingTop,
+              paddingBottom: buttonProps.paddingBottom,
+              fontSize: buttonProps.fontSize,
+            }}
+            type="primary"
+            onClick={handleLogin}
+          >
             Get Started
           </Button>
         );
       }
       if (role) {
         return (
-          <Button type="primary">
+          <Button
+            style={{
+              paddingInline: buttonProps.paddingInline,
+              paddingTop: buttonProps.paddingTop,
+              paddingBottom: buttonProps.paddingBottom,
+              fontSize: buttonProps.fontSize,
+            }}
+            shape="round"
+            type="primary"
+          >
             <Link href={`/dashboard/${role.toLowerCase()}`}>
               Go to Dashboard
             </Link>
@@ -134,19 +162,47 @@ export default function Home() {
       }
       if (accountSetupIncomplete) {
         return (
-          <Button type="primary">
+          <Button
+            shape="round"
+            type="primary"
+            style={{
+              paddingInline: buttonProps.paddingInline,
+              paddingTop: buttonProps.paddingTop,
+              paddingBottom: buttonProps.paddingBottom,
+              fontSize: buttonProps.fontSize,
+            }}
+          >
             <Link href="/onboarding">Complete Account Setup</Link>
           </Button>
         );
       }
       return (
-        <Button type="primary">
+        <Button
+          shape="round"
+          type="primary"
+          style={{
+            paddingInline: buttonProps.paddingInline,
+            paddingTop: buttonProps.paddingTop,
+            paddingBottom: buttonProps.paddingBottom,
+            fontSize: buttonProps.fontSize,
+          }}
+        >
           <Link href="/onboarding">Complete Account Setup</Link>
         </Button>
       );
     }
     return (
-      <Button type="primary" loading>
+      <Button
+        style={{
+          paddingInline: buttonProps.paddingInline,
+          paddingTop: buttonProps.paddingTop,
+          paddingBottom: buttonProps.paddingBottom,
+          fontSize: buttonProps.fontSize,
+        }}
+        shape="round"
+        type="primary"
+        loading
+      >
         Loading
       </Button>
     );
@@ -155,37 +211,146 @@ export default function Home() {
   // Renders the appropriate button based on the user's authentication and role status.
 
   return (
-    <>
+    <div
+      style={{ height: "100vh", width: "100vw", backgroundColor: "#ffffff" }}
+    >
+      <div
+        style={{
+          width: "256px",
+          height: "256px",
+          borderRadius: "100%",
+          backgroundColor: "#37F5FF",
+          filter: "blur(132px)",
+          flexShrink: 0,
+          position: "absolute",
+          top: "34px",
+          left: "-36px",
+          zIndex: 0,
+        }}
+      ></div>
+      <div
+        style={{
+          width: "256px",
+          height: "256px",
+          borderRadius: "100%",
+          backgroundColor: "#71C9FE",
+          filter: "blur(132px)",
+          flexShrink: 0,
+          position: "absolute",
+          top: "96px",
+          right: "-24px",
+          zIndex: 0,
+        }}
+      ></div>
+      <div
+        style={{
+          width: "256px",
+          height: "256px",
+          borderRadius: "100%",
+          backgroundColor: "#A2B9FF",
+          filter: "blur(132px)",
+          flexShrink: 0,
+          position: "absolute",
+          top: "0px",
+          right: "0px",
+          zIndex: 0,
+        }}
+      ></div>
       {contextHolder}
       <Layout
         style={{
           height: "100vh",
           width: "100vw",
-          backgroundColor: "#202020",
+          backgroundColor: "transparent",
+          zIndex: 2,
         }}
       >
         <Header
           style={{
             display: "flex",
+            width: "100%",
+            padding: "1.2em 6em",
             justifyContent: "space-between",
             alignItems: "center",
-            backgroundColor: "transparent",
-            paddingTop: 24,
-            paddingLeft: 96,
-            paddingRight: 96,
-            paddingBottom: 24,
+            backgroundColor: "rgba(255, 255, 255, 0.07)",
+            borderBottom: "1px solid #f0f0f0",
+            backdropFilter: "blur(2px)",
+            height: "fit-content",
+            zIndex: 1000,
+          }}
+          className="home-header"
+        >
+          <Image
+            src={hctlogo}
+            alt="1HCT Logo"
+            style={{ width: "auto" }}
+            height={36}
+          />
+          {renderButton({
+            paddingInline: "2.4em",
+            paddingTop: "1.5em",
+            paddingBottom: "1.5em",
+            fontSize: "1em",
+          })}
+        </Header>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            paddingTop: "8em",
+            paddingLeft: "2em",
+            paddingRight: "2em",
+            height: "100%",
           }}
         >
-          <Menu
-            mode="horizontal"
-            style={{ flex: 1, backgroundColor: "transparent" }}
+          <div
+            style={{
+              padding: "0.6em 1.8em",
+              backgroundColor: "rgba(255,255,255, 0.56)",
+              border: "1px solid #f0f0f0",
+              borderRadius: "70px",
+              margin: "1em 1.8em",
+              backdropFilter: "blur(2px)",
+              textAlign: "center",
+              color: "black",
+              fontSize: "1.2em",
+              width: "fit-content",
+            }}
           >
-            <Menu.Item key="1" style={{ color: "white" }}>
-              <Link href="/">Home</Link>
-            </Menu.Item>
-          </Menu>
-          {renderButton()}
-        </Header>
+            World&apos;s Most Adopted Platform by Healthcare Professionals
+          </div>
+          <div
+            style={{
+              color: "black",
+              fontSize: "5em",
+              fontWeight: 500,
+              paddingTop: 20,
+              letterSpacing: "-0.01em",
+              textAlign: "center",
+              lineHeight: "1.15em",
+            }}
+          >
+            The Leading Platform For <br />
+            Managing Careworkers
+          </div>
+          <div style={{ paddingTop: "6em" }}>
+            {renderButton({
+              paddingInline: "3em",
+              paddingTop: "1.5em",
+              paddingBottom: "1.5em",
+              fontSize: "1.2em",
+            })}
+          </div>
+          <div
+            style={{
+              height: 1,
+              width: "85%",
+              backgroundColor: "#EBF0F5",
+              marginTop: "12em",
+            }}
+          ></div>
+        </div>
         {user && (
           <div
             style={{
@@ -201,6 +366,6 @@ export default function Home() {
           </div>
         )}
       </Layout>
-    </>
+    </div>
   );
 }
